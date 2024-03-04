81db7f1be4802a441d58de24200c3304f51dca6c
xb <2366733570@qq.com>
2024-03-04 17:32:22 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+import json
+from custom_error import CustomError
+app = Flask(__name__)
+
+
+@app.route('/', methods=("GET", "POST"))
+def Ipv6Info():  # put application's code here
+    # 接受客户端提交的IPv6地址，
+    try:
+        if request.method == "POST":
+            data_json = json.loads(request.data)
+            uid = str(data_json['uid'])
+            IPv6Address = str(data_json['IPv6Address'])
+            domain = str(data_json['domain'])
+            if len(uid.strip()) == 0 or len(IPv6Address.strip()) == 0:
+                return '{"code": -1, "messages": "uid和ipv6地址不能为空！"}'
+            print(data_json['uid'])
+            # print(data)
+            raise CustomError("11212")
+
+        else:
+            return '{"code": -1, "messages": "请求类型错误！"}'
+    except CustomError as e:
+        print(e)
+
+    except Exception as e:
+        error_json = json.loads('{"code": -1, "messages": ""}')
+        error_json['messages'] = e
+        return
+    # 记录好时间，精确到秒，
+    # 支持客户端提交一个域名，需校验必须唯一，如果不提交域名，服务器自动生成域名
+    # 每次客户端请求，都把所有最新的IP拼域名，根据主机进行分组过滤，返回给客户端，客户端写入hosts文件
+    # 数据需要存到数据库
+
+if __name__ == '__main__':
+    app.run()
