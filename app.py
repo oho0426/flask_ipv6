3f042ae3b3f3e53a322e9aeda7e9bbab5a9f6543
xb <1436329621@qq.com>
2024-03-04 21:11:24 +0800

    增加数据库配置，完善域名校验

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask import request
 import json
+import random
+import string
+
+from sqlite_database import SQLiteDatabase
 app = Flask(__name__)
 
+
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits  # 包含大小写字母和数字
+    return ''.join([random.choice(letters) for _ in range(length)])
+    # 调用函数生成长度为10的随机字符串
 
 @app.route('/', methods=("GET", "POST"))
 def Ipv6Info():  # put application's code here
@@ -23,11 +32,25 @@ def Ipv6Info():  # put application's code here
             if len(uid.strip()) == 0 or len(IPv6Address.strip()) == 0:
                 raise ValueError("uid和ipv6地址不能为空！")
 
-            # domain格式校验，
+            # domain格式校验，域名起码要有两段，如果没传域名，就随机生成一个
             if len(domain.strip()) != 0:
                 domain_list = domain.split(".")
                 if len(domain_list) < 2 or len(domain_list) > 3:
                     raise ValueError("域名格式错误！(示例：www.wb.com/wb.com)")
+            else:
+                while(True):
+                    result = generate_random_string(5)
+                    where_str = 'domain = %s' % result
+                    database = SQLiteDatabase('example.db')
+                    ipv6_info = database.get_where_data(where_str)
+                    if len(ipv6_info) == 0:
+                        domain = result + ".ipv6.com"
+                        break
+
+            print(domain)
+
+
+
 
 
 
@@ -43,11 +66,11 @@ def Ipv6Info():  # put application's code here
         error_json['messages'] = str(e)
         return error_json
 
-    except Exception:
-        error_json = json.loads('{"code": -1, "messages": ""}')
-        error_json['messages'] = "未知错误！"
-
-        return error_json
+    # except Exception:
+    #     error_json = json.loads('{"code": -1, "messages": ""}')
+    #     error_json['messages'] = "未知错误！"
+    #
+    #     return error_json
 
 
 if __name__ == '__main__':
---
+++ sqlite_database.py
@@ -0,0 +1,72 @@
+import sqlite3
+
+
+class SQLiteDatabase:
+    def __init__(self, db_name):
+        self.db = None
+        try:
+            # 连接到指定名称的SQLite数据库文件（如果不存在则创建）
+            self.db = sqlite3.connect(db_name)
+
+            # 创建表格（可根据需要自行修改）
+            cursor = self.db.cursor()
+            create_table_query = '''CREATE TABLE IF NOT EXISTS ipv6_info (id INTEGER PRIMARY KEY AUTOINCREMENT, `uid` VARCHAR(50) NOT NULL, `ipv6address` VARCHAR(50) NOT NULL, `domain` VARCHAR(50), `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP);'''
+            cursor.execute(create_table_query)
+            self.db.commit()
+
+        except Exception as e:
+            print("Error connecting to database:", str(e))
+
+    def insert_data(self, data):
+        try:
+            cursor = self.db.cursor()
+            query = "INSERT INTO ipv6_info(`uid`,`ipv6address`,`domain`) VALUES(?, ?, ?)"
+            values = list(data.values())
+            values_tp = tuple(values)
+            cursor.execute(query, values_tp)
+            self.db.commit()
+            return True
+
+        except Exception as e:
+            print("Error inserting data into table:", str(e))
+            return False
+
+    def get_all_data(self):
+        try:
+            cursor = self.db.cursor()
+            query = "SELECT * FROM ipv6_info"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+        except Exception as e:
+            print("Error retrieving all data from the table:", str(e))
+            return []
+
+    def get_where_data(self, where_str):
+        try:
+            cursor = self.db.cursor()
+            query = "SELECT * FROM ipv6_info WHERE ?"
+            cursor.execute(query, where_str)
+            result = cursor.fetchall()
+            return result
+
+        except Exception as e:
+            print("Error retrieving data from the table:", str(e))
+            return []
+
+    def close_connection(self):
+        if self.db is not None:
+            self.db.close()
+
+
+
+if __name__ == '__main__':
+    # 测试代码
+    database = SQLiteDatabase('example.db')
+    user1 = {'uid': 1, 'ipv6': "2409:8a56:3227:514:a4f0:cb52:1a78:1ee0", 'domain': "111.com"}
+    user2 = {'uid': 2, 'ipv6': '2429:8a56:3227:514:a4f0:cb52:1a78:1ee0', 'domain': "1"}
+    database.insert_data(user1)
+    database.insert_data(user2)
+    result = database.get_all_data()
+    print(result)
+    database.close_connection()
